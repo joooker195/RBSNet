be308e4c15c00960e971b55acd75232a433b2f72
joooker195 <joooker195@gmail.com>
2017-05-05 04:31:45 +0400

    new model

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import model.impl.RBFModelNetwork;
+import model2.ModelNetwork;
+import model2.Neuron;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ксю on 16.04.2017.
@@ -7,8 +11,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        RBFModelNetwork model = new RBFModelNetwork(4, 5);
-        model.createNetwork();
+       // RBFModelNetwork model = new RBFModelNetwork(4, 5);
+        ModelNetwork model = new ModelNetwork(4, 3);
+        ArrayList<ArrayList<Neuron>> network =  model.createNetwork();
+
 
     }
 }
