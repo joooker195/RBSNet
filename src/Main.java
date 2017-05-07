2d6192cdbbb1a492aa244ac3cfb0e2968856b4a3
joooker195 <joooker195@gmail.com>
2017-05-07 23:37:10 +0400

    commit Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-import model.impl.RBFModelNetwork;
-import model2.ModelNetwork;
-import model2.Neuron;
+import model.DataExchange;
+import model.Function;
+import model.ModelNetwork;
+import model.Neuron;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -9,12 +11,19 @@ import java.util.ArrayList;
  */
 public class Main
 {
-    public static void main(String[] args)
-    {
-       // RBFModelNetwork model = new RBFModelNetwork(4, 5);
-        ModelNetwork model = new ModelNetwork(4, 3);
-        ArrayList<ArrayList<Neuron>> network =  model.createNetwork();
-
-
+    static int n = 1000000;
+    public static void main(String[] args) throws IOException {
+        // RBFModelNetwork model = new RBFModelNetwork(4, 5);
+        ModelNetwork model = new ModelNetwork(7, 5);
+        ArrayList<ArrayList<Neuron>> network = model.createNetwork();
+        Function f = new Function(network);
+        ArrayList<Double> data = DataExchange.readFromExcel("dataBrent.xls", "Лист1");
+        f.learn(data);
+        ArrayList<Double> datatest = DataExchange.readFromExcel("dataTestBrent.xls", "Лист1");
+        f.testing(datatest);
+       DataExchange.writeToExcel("2.xlsx");
     }
+
+
+
 }
